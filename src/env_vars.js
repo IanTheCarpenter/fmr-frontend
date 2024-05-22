f46928a412f232fa078c590e043e51053084b6ae
Ian Carpenter <ian@carpenter.cx>
2024-05-22 17:01:37 -0500

    added https

--- src/env_vars.js
+++ src/env_vars.js
@@ -1,5 +1,5 @@
 module.exports = {
-    URL: "http://fmr-backend.vercel.app/api/v1/ziplookup"
+    URL: "https://fmr-backend.vercel.app/api/v1/ziplookup"
 }
 // module.exports = {
 //     URL: "http://localhost:3001/api/v1/ziplookup"
