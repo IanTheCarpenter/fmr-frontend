69220600f1e70cf3c25ad6919ddaa0fa4f2195cc
Ian Carpenter <ian@carpenter.cx>
2024-06-22 17:17:55 -0500

    re-wrote description, tweaked scaling

--- src/components/description.js
+++ src/components/description.js
@@ -4,7 +4,9 @@ export default function Description() {
   return (
     <div class='description'> 
         <h3>What's this??</h3>
-        <>This is what it takes to comfortably live in the given Zip code</>
+        <>
+          Conventional wisdom states that it is wise to keep your rent payment to no more than 1/3 of your income. <br/><br/> Enter a zip code in the bar above to see how much hourly wage is needed to afford a 1-bedroom apartment, assuming a 40 hour work week. This should represent the bare minimum to comfortably afford to house a small family.
+        </>
     </div>
   )
 }
